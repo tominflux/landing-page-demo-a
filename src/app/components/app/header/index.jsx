26a90a000741e0b85c562a0e5c8778ba24d689c9
Tom Chesters <tom@tomaaato.xyz>
2020-10-31 23:59:24 +0000

    Header - Layout Improvements

--- src/app/components/app/header/index.jsx
+++ src/app/components/app/header/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import HeaderOption from './option'
 import HeaderLogo from './logo'
+import classConcat from '../../../misc/classConcat'
 import './styles.css'
 
 const Header = () => (
-    <header className="header">
-        <div className="row">
-            <div className="col-4 col-md-2 text-center">
+    <header className={classConcat(
+        "container-fluid",
+        "p-0",
+        "p-md-3",
+        "header"
+    )}>
+        <div className="row align-items-center">
+            <div className="col-4 col-md-2 col-lg-2 text-center">
                 <HeaderLogo />
             </div>
-            <div className="offset-md-3"/>
-            <div className="col-8 col-md-7 px-3">
+            <div className="offset-md-1 offset-lg-3"/>
+            <div className="d-none d-md-block col-md col-lg-7 px-3">
                 <div className="row align-items-center">
                     <div className="col-3">
                         <HeaderOption>
--- src/app/components/app/header/logo/index.jsx
+++ src/app/components/app/header/logo/index.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import logoSvg from './orbits.svg'
 import './styles.css'
 
-const HeaderLogo = (props) => {
-    //
-    return (
-        <img 
-            className="header__logo"
-            src={logoSvg}
-            alt="Orbits icon"
-        />
-    )
-}
+const HeaderLogo = () => (
+    <img 
+        className="header__logo"
+        src={logoSvg}
+        alt="Orbits icon"
+    />
+)
 
 export default HeaderLogo
